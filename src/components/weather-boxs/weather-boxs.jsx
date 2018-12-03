0d04f073d49aeea05ded83318049a4e075645708
HBKarim2018 <44598175+HBKarim2018@users.noreply.github.com>
2018-12-03 03:58:53 +0200

    Update weather-boxs.jsx

--- src/components/weather-boxs/weather-boxs.jsx
+++ src/components/weather-boxs/weather-boxs.jsx
@@ -7,7 +7,7 @@ class WeatherBoxs extends Component {
         <hr />
         <ul className="row list-unstyled p-3 text-center text-light">
           <li className="col-lg-3 col-md-6 col-sm-12 p-2">
-            <div className="w-100 bg-dark p-2 border-radius">
+            <div className="w-100 bg-dark p-2 border-radius py-5">
               <h6 className="font-family-head head-font-size">Location :</h6>
               <h1 className="font-family-normal head-font-size-high mb-0">
                 {this.props.city} , {this.props.country}{" "}
@@ -16,7 +16,7 @@ class WeatherBoxs extends Component {
             </div>
           </li>
           <li className="col-lg-3 col-md-6 col-sm-12 p-2">
-            <div className="w-100 bg-dark p-2 border-radius">
+            <div className="w-100 bg-dark p-2 border-radius py-5">
               <h6 className="font-family-head head-font-size">Temperature :</h6>
               <h1 className="font-family-normal head-font-size-high mb-0">
                 {this.props.temp}{" "}
@@ -25,7 +25,7 @@ class WeatherBoxs extends Component {
             </div>
           </li>
           <li className="col-lg-3 col-md-6 col-sm-12 p-2">
-            <div className="w-100 bg-dark p-2 border-radius">
+            <div className="w-100 bg-dark p-2 border-radius py-5">
               <h6 className="font-family-head head-font-size">Humidity :</h6>
               <h1 className="font-family-normal head-font-size-high mb-0">
                 {this.props.humidity} <i className="fa text-muted fa-tint" />
@@ -33,7 +33,7 @@ class WeatherBoxs extends Component {
             </div>
           </li>
           <li className="col-lg-3 col-md-6 col-sm-12 p-2">
-            <div className="w-100 bg-dark p-2 border-radius">
+            <div className="w-100 bg-dark p-2 border-radius py-5">
               <h6 className="font-family-head head-font-size">Description :</h6>
               <h1 className="font-family-normal head-font-size-high mb-0">
                 {this.props.description}
