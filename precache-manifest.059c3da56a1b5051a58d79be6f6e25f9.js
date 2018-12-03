78b41d1246014f9bb90e645770d1d8779f3eb22b
karim ElMelayati <karimElmelayati@gmail.com>
2018-12-03 03:49:33 +0200

    Updates

---
+++ precache-manifest.059c3da56a1b5051a58d79be6f6e25f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "802b8c34e84fb6e8281f",
+    "url": "/weather/static/css/main.c2dc5a9f.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "03581f75987386150d88",
+    "url": "/weather/static/js/1.03581f75.chunk.js"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "802b8c34e84fb6e8281f",
+    "url": "/weather/static/js/main.802b8c34.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "03581f75987386150d88",
+    "url": "/weather/static/css/1.aea5d378.chunk.css"
+  },
+  {
+    "revision": "3ba2ff96cadc649c4aef185184701332",
+    "url": "/weather/index.html"
+  }
+];
